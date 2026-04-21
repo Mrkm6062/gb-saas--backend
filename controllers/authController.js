5010c4116dd5d8333c5d493c04d31f430b9f6195
Mrkm6062 <akm222143@gmail.com>
2026-04-21 22:46:16 +0530

    login issue is resolved 2.0

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,12 +21,8 @@ export const registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // 3. Hash the password before saving
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
     // Create user
-    const user = await User.create({ name, email: normalizedEmail, password: hashedPassword });
+    const user = await User.create({ name, email: normalizedEmail, password });
 
     res.status(201).json({
       _id: user._id,
