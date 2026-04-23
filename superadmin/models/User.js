d4459fddb5b2cc2d0de0002bff913634deb8f151
Mrkm6062 <akm222143@gmail.com>
2026-04-23 16:34:11 +0530

    update user.js issue resolved

---
+++ superadmin/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    userId: { type: String, unique: true },
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    role: { type: String, enum: ['user', 'superadmin'], default: 'user' },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
