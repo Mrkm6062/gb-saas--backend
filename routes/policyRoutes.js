5d073b10e7a3cf0e3fd454c727a526034e777d76
Mrkm6062 <akm222143@gmail.com>
2026-04-25 00:04:06 +0530

    updated policy feature 2.0

--- routes/policyRoutes.js
+++ routes/policyRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getPolicies, createPolicy, updatePolicy, deletePolicy } from "../controllers/policyController.js";
+import { getPolicies, createPolicy, updatePolicy, deletePolicy, getPublicPolicies } from "../controllers/policyController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import { subdomainMiddleware } from "../middleware/subdomain.js";
+import { storeResolver } from "../middleware/storeResolver.js";
 
 const router = express.Router();
+
+// Public storefront route (protected by subdomain detection)
+router.get("/public", subdomainMiddleware, storeResolver, getPublicPolicies);
 
 router.get("/", protect, getPolicies);
 router.post("/", protect, createPolicy);
