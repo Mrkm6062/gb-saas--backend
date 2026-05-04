2004690e6465bbec38d631916bdc8110327142b3
Mrkm6062 <akm222143@gmail.com>
2026-05-04 09:35:11 +0530

    CSP issue resolved for custom domain

--- Server.js
+++ Server.js
@@ -61,11 +61,11 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        imgSrc: ["'self'", "https://storage.googleapis.com", "data:"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "https://storage.googleapis.com", "data:", "blob:"],
+        scriptSrc: ["'self'", "'unsafe-inline'", "https://static.cloudflareinsights.com"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
         fontSrc: ["'self'", "https://fonts.gstatic.com", "data:"],
-        connectSrc: ["'self'", "https://*.galibrand.cloud"],
+        connectSrc: ["'self'", "https://*.galibrand.cloud", "https://cloudflareinsights.com"],
       },
     },
   })
