a893eb2ee4af0968c92f469dd1a97e406c6d6e63
Mrkm6062 <akm222143@gmail.com>
2026-05-07 15:46:19 +0530

    updated for payment

--- superadmin/services/paymentService.js
+++ superadmin/services/paymentService.js
@@ -1,5 +1,6 @@
 import Razorpay from "razorpay";
 import PlatformPaymentSettings from "../../models/PlatformPaymentSettings.js";
+import { decrypt } from "../../utils/crypto.js";
 
 const getRazorpayInstance = async () => {
   const settings = await PlatformPaymentSettings.findOne();
@@ -8,9 +9,14 @@ const getRazorpayInstance = async () => {
     throw new Error("Razorpay keys missing in platform settings or gateway is disabled");
   }
 
+  const decryptedSecret = decrypt(settings.razorpayKeySecret);
+  if (!decryptedSecret) {
+    throw new Error("Failed to decrypt Razorpay secret key. It may be corrupted or the ENCRYPTION_KEY is wrong.");
+  }
+
   return new Razorpay({
     key_id: settings.razorpayKeyId,
-    key_secret: settings.razorpayKeySecret,
+    key_secret: decryptedSecret,
   });
 };
 
