8d8aa2343f27c569a070d399e3b8da637c801d61
Mrkm6062 <akm222143@gmail.com>
2026-06-04 08:53:00 +0530

    issue resolved cors issue

--- Server.js
+++ Server.js
@@ -68,7 +68,7 @@ app.use((req, res, next) => {
   if (origin) res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-store-id, x-store-code, x-store-domain, Origin, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-store-id, x-store-code, x-store-domain, x-forwarded-host, Origin, Accept');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
