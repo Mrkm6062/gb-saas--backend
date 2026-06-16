e5005e1b48cd7275d51e43716f0d61ddcfdccc1c
Mrkm6062 <akm222143@gmail.com>
2026-06-16 14:07:11 +0530

    issue resolved of store not found

--- Server.js
+++ Server.js
@@ -71,7 +71,7 @@ app.use((req, res, next) => {
   if (origin) res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-store-id, x-store-code, x-store-domain, x-forwarded-host, Origin, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-store-id, x-store, x-store-code, x-store-domain, x-forwarded-host, Origin, Accept');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
@@ -108,8 +108,7 @@ app.use(
         connectSrc: ["'self'", "https:", "http:"],
         frameSrc: ["'self'", "https://checkout.razorpay.com", "https://api.razorpay.com"],
       },
-    },
-    contentSecurityPolicy: false,
+    }
   })
 );
 
