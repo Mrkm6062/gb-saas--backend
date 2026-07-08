29bcaf6939590c1b4a61250b360b51c62f89783c
Mrkm6062 <akm222143@gmail.com>
2026-07-08 18:37:29 +0530

    issue resolved

--- Server.js
+++ Server.js
@@ -86,7 +86,7 @@ app.use((req, res, next) => {
   if (origin) res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-store-id, x-store, x-store-code, x-store-domain, x-forwarded-host, Origin, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-store-id, x-store, x-store-code, x-store-domain, x-forwarded-host, Origin, Accept, x-csrf-token');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
